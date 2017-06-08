5b53ece43b1b9ab0f55b1f88869882df7f428fd0
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-08 02:03:19 -0400

    Python admin tool ready to use

--- activity_finder/admin.py
+++ activity_finder/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Activity,ActivityType,CommunityPartner,Course,FocusArea,Location,People,Population,School,ServedNeighbourhood,Unit
 
 # Register your models here.
+admin.site.register(Activity)
+admin.site.register(ActivityType)
+admin.site.register(CommunityPartner)
+admin.site.register(Course)
+admin.site.register(FocusArea)
+admin.site.register(Location)
+admin.site.register(People)
+admin.site.register(Population)
+admin.site.register(ServedNeighbourhood)
+admin.site.register(School)
+admin.site.register(Unit)
+
--- activity_finder/models.py
+++ activity_finder/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-
 class ActivityType(models.Model):
     idActivityType = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
