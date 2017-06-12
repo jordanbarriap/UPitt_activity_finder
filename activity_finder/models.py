8057294a207a95a0548b5c242673d808dcefe035
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-12 10:49:43 -0400

    DB models modified and Django admin improved
    Now names of entities are shown instead of e.g. “People object”

--- activity_finder/models.py
+++ activity_finder/models.py
@@ -5,6 +5,9 @@ class ActivityType(models.Model):
     idActivityType = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class People(models.Model):
     idPeople = models.AutoField(primary_key=True)
@@ -12,6 +15,85 @@ class People(models.Model):
     last_name = models.CharField(max_length=45)
     email = models.CharField(max_length=45)
     type = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.first_name+" "+self.last_name
+
+
+class FocusArea(models.Model):
+    idFocusArea = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    idLocation = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    idSchool = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Unit(models.Model):
+    idUnit = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    idSchool = models.ForeignKey(School, on_delete=models.CASCADE)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+class Population(models.Model):
+    idPopulation= models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+
+class ServedNeighbourhood(models.Model):
+    idServedNeighbourhood = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    idCourse = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
+
+class CommunityPartner(models.Model):
+    idCommunityPartner = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    #activities = models.ManyToManyField(Activity)
+
+    def __str__(self):
+        return self.name
+
 
 
 class Activity(models.Model):
@@ -26,58 +108,18 @@ class Activity(models.Model):
     funding = models.CharField(max_length=500)
     reviewed = models.CharField(max_length=100)
     unitNotes = models.CharField(max_length=500)
-    idPeople = models.ForeignKey(People, on_delete=models.CASCADE,related_name="in_charge_person")
+    idPeople = models.ForeignKey(People, on_delete=models.CASCADE,related_name="in_charge_person", null = True)
     universityLeaders = models.ManyToManyField(People,related_name="university_leader")
+    idFocusArea = models.ForeignKey(FocusArea, on_delete=models.CASCADE, null = True)
+    idLocation = models.ForeignKey(Location, on_delete=models.CASCADE, null = True)
+    idUnit = models.ForeignKey(Unit, on_delete=models.CASCADE, null = True)
+    idPopulation = models.ForeignKey(Population, on_delete=models.CASCADE, null = True)
+    idServedNeighbourhood = models.ForeignKey(ServedNeighbourhood, on_delete=models.CASCADE, null = True)
+    idCourse = models.ForeignKey(Course, on_delete=models.CASCADE, null = True)
+    idCommunityPartner = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE, null = True)
 
-
-class FocusArea(models.Model):
-    idFocusArea = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    activities = models.ManyToManyField(Activity)
-
-
-class Location(models.Model):
-    idLocation = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    activities = models.ManyToManyField(Activity)
-
-
-class School(models.Model):
-    idSchool = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-
-
-class Unit(models.Model):
-    idUnit = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    idSchool = models.ForeignKey(School, on_delete=models.CASCADE)
-    activities = models.ManyToManyField(Activity)
-
-
-class Population(models.Model):
-    idPopulation= models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    activities = models.ManyToManyField(Activity)
-
-
-class ServedNeighbourhood(models.Model):
-    idServedNeighbourhood = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    activities = models.ManyToManyField(Activity)
-
-
-class Course(models.Model):
-    idCourse = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    activities = models.ManyToManyField(Activity)
-
-
-class CommunityPartner(models.Model):
-    idCommunityPartner = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    activities = models.ManyToManyField(Activity)
+    def __str__(self):
+        return self.name
 
 
 
