d3ced80f6f9ecb9b11733d500a6624a045cb86d0
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-14 07:50:02 -0400

    Primitive html page for the project

--- activity_finder/views.py
+++ activity_finder/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def activity_finder(request):
+    return render(request, 'index.html', {})
