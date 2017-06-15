a220b033a3bd340830e3e222427c32ae5a5c5570
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-15 19:17:27 -0400

    Reports version 2

--- reports/views.py
+++ reports/views.py
@@ -2,8 +2,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
-from activity_finder.models import Activity
+#from activity_finder.models import Activity
 
-def activity_finder(request):
-    activities=Activity.objects.all()
+def reports(request):
+    #activities=Activity.objects.all()
     return render(request, 'reports.html', {})
