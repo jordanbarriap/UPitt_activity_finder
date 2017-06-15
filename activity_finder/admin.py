2d14006c8e9270e604ae4b451949df64254ed69f
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-15 18:23:09 -0400

    Test commit

--- activity_finder/admin.py
+++ activity_finder/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Activity,ActivityType,CommunityPartner,Course,FocusArea,Location,People,Population,School,ServedNeighbourhood,Unit
 
+from django.forms import TextInput, Textarea
+from django.db import models
+
 # Register your models here.
 admin.site.register(Activity)
 admin.site.register(ActivityType)
@@ -13,4 +16,12 @@ admin.site.register(Population)
 admin.site.register(ServedNeighbourhood)
 admin.site.register(School)
 admin.site.register(Unit)
+
+'''class ActivityModelAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        models.TextField: {'widget': Textarea(attrs={'rows':4, 'cols':100})},
+    }
+
+admin.site.register(Activity, ActivityModelAdmin)'''
 
