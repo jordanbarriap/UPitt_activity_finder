92882048362ac374ea9d1067a694a428ed4761ae
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-15 17:50:54 -0400

    contactInformation added as Boolean

--- activity_finder/models.py
+++ activity_finder/models.py
@@ -124,6 +124,7 @@ class Activity(models.Model):
     courses = models.ManyToManyField(Course, related_name="associated_course")
     #idCommunityPartner = models.ForeignKey(CommunityPartner, on_delete=models.CASCADE, null = True)
     communityPartners = models.ManyToManyField(CommunityPartner, related_name="associated_community_partner")
+    contactInformation= models.BooleanField()
 
     def __str__(self):
         return self.name
