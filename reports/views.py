0c61952dee0f5c3473547d20fff0f0b11425c11e
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-16 17:47:49 -0400

    getEntity function 1st version

--- reports/views.py
+++ reports/views.py
@@ -12,3 +12,21 @@ def reports(request):
     for entity in entities:
         entities_list.append(entity.__name__)
     return render(request, 'reports.html', {'entities':entities_list})
+
+def get_entity(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = EntityForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            #return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    #else:
+        #form = NameForm()
+
+    return render(request, 'name.html', {'form': form})
