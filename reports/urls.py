d89eabae409ceb5cd75069e48fbd38dbdb33332a
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-06-23 14:46:49 -0400

    Filters added and table allows filtering
    NOTE: It just works for only 2 combinations and just for ManyToMany
    fields

--- reports/urls.py
+++ reports/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.reports),
     url(r'^get_filters$', views.get_filters),
     url(r'^get_subfilters$', views.get_subfilters),
-    url(r'^get_table$', views.get_table)
+    url(r'^get_table$', views.get_table),
+    url(r'^get_subtable$', views.get_subtable)
 ]
--- reports/views.py
+++ reports/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse, JsonResponse
 from django.forms.models import model_to_dict
 from django.core import serializers
 import datetime
+import copy
+from itertools import chain
 from activity_finder.models import Activity
 
 from django.apps import apps
@@ -17,14 +19,22 @@ def reports(request):
 
 
 def get_filters(request):
-    entity = request.GET.get('entity', None)
-    act_attributes = Activity._meta.fields
+    selected_entity = request.GET.get('entity', None)
+    '''act_attributes = Activity._meta.fields
     attributes = []
     for act in act_attributes:
         if type(act).__name__ != 'AutoField':
             attributes.append(act.verbose_name)
     data = {
         'filters':attributes
+    }'''
+    entities = apps.get_app_config('activity_finder').get_models(include_auto_created=False)
+    entities_list = []
+    for entity in entities:
+        if entity.__name__!=selected_entity:
+            entities_list.append(entity.__name__)
+    data = {
+        'filters': entities_list
     }
     return JsonResponse(data)
 
@@ -37,12 +47,14 @@ def get_table(request):
     json_data = []
     columns = []
     data_types = []
+    data_filters = []
     row=1
     for value in query_values:
         if len(columns)==0:
             columns = list(value.keys())
             for column in columns:
                 data_types.append(type(column).__name__)
+                data_filters.append("#select_filter")
             print(data_types)
         json_value={'id':row, 'data':list(value.values())}
         print(json_value)
@@ -52,28 +64,95 @@ def get_table(request):
     data = {
         'columns':columns,
         'types':list(data_types),
+        'filters':list(data_filters),
         'data':{'rows': json_data}
     }
 
     return JsonResponse(data)
 
 def get_subfilters(request):
-    entity = request.GET.get('entity', None)
-    field = request.GET.get('field', None)
-    act_values=[]
-    if entity=='Activity':
+    entity1 = request.GET.get('entity1', None)
+    entity2 = request.GET.get('entity2', None)
+    data=[]
+    #act_values=[]
+    if entity1!=entity2:
+        entity_model1 = apps.get_model(app_label='activity_finder', model_name=entity1)
+        entity_model2 = apps.get_model(app_label='activity_finder', model_name=entity2)
+        queryset1 = entity_model1.activitys.all()
+        print(queryset1)
+    '''if entity=='Activity':
         field_type = type(Activity._meta.get_field(field)).__name__
         if field_type=='ForeignKey':
             queryset = Activity.objects.values(field+"__name")
             act_values = [entry for entry in queryset]
         else:
-            act_values = Activity.objects.values(field)
-    print(act_values)
+            act_values = Activity.objects.values(field)'''
+    #print(act_values)
     #attributes = []
     #for act in act_attributes:
     #    if type(act).__name__ != 'AutoField':
     #        attributes.append(act.verbose_name)
+    #data = {
+    #    'values': act_values
+    #}
+    return JsonResponse(data)
+
+def get_subtable(request):
+    entity1 = request.GET.get('entity1', None)
+    entity2 = request.GET.get('entity2', None)
+
+    data=[]
+    queryset1=[]
+    query_values = []
+    data_types = []
+    data_filters = []
+    columns = []
+    json_data = []
+    row = 0
+    ent1_count=0
+    rows = []
+    if entity1!=entity2:
+        entity_model1 = apps.get_model(app_label='activity_finder', model_name=entity1)
+        entity_model2 = apps.get_model(app_label='activity_finder', model_name=entity2)
+
+        queryset1 = entity_model1.objects.all()#objects.all().prefetch_related('activity_set')
+
+        queryvalues = []
+        queryset1_values = entity_model1.objects.all().values()#[entry for entry in queryset1]
+
+        for ent1 in queryset1:
+            queryset2 = ent1.activity_set.all()
+            ent1_values = queryset1_values[ent1_count]
+
+            for ent2 in queryset2:
+                entity2_lowercase = entity2.lower()
+                queryset3 = getattr(ent2, entity2_lowercase+"s").all()
+
+                for ent3 in queryset3:
+
+                    rows.append(copy.copy(ent1_values))
+                    rows[row][entity2]=str(ent3)
+                    #print(rows[row])
+                    row = row + 1
+            ent1_count = ent1_count + 1
+
+    print(rows)
+    query_values = [entry for entry in rows]
+    for value in query_values:
+        if len(columns)==0:
+            columns = list(value.keys())
+            for column in columns:
+                data_types.append(type(column).__name__)
+                data_filters.append("#select_filter")
+            print(data_types)
+        json_value={'id':row, 'data':list(value.values())}
+
+        json_data.append(json_value)
+        row = row + 1
     data = {
-        'values': act_values
+        'columns':columns,
+        'types':list(data_types),
+        'filters':list(data_filters),
+        'data':{'rows': json_data}
     }
     return JsonResponse(data)
