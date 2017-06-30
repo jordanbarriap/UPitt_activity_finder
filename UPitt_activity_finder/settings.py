3caad71c8eb9bb00a140ed089ff3747c5ccd8e2b
HUNG CHAU <hungchaukim@gmail.com>
2017-06-30 18:26:21 -0400

    put the template folder back to activity_finder

--- UPitt_activity_finder/settings.py
+++ UPitt_activity_finder/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'UPitt_activity_finder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'activity_finder/templates')],
         # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
