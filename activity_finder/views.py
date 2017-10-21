03562a6b4758e8813825d96c2f4b5266009baf33
jordanbarriap <PAWSRES1@PAWSs-MacBook-Pro.local>
2017-10-21 14:29:05 -0400

    Fronted changes (map-related and style)
    - Webpage description added
    - Changes in style (smaller fonts)
    - Click on a location filter activities and show the ones that just
    take place there
    - Click on the map undo a previous click done on a specific marker
    - Mouseover on a marker show a popup with the activities having place
    there and its address

--- activity_finder/views.py
+++ activity_finder/views.py
@@ -9,7 +9,16 @@ def activity_finder(request):
     data['activities']=Activity.objects.all()
     # print(data['activities'])
     data['locations']=Location.objects.all()
-    # print(data['locations'])
+    #print(data['locations'])
+    for location in data['locations']:
+        location_id = location.idLocation
+        #print("Location id: "+str(location_id))
+        activities = Activity.objects.filter(locations=location_id)
+        activities_array = []
+        for activity in activities:
+            activities_array.append(activity)
+        location.activities = activities_array
+
     data['activitytypes']=ActivityType.objects.all()
 
     return render(request, 'index.html', data)
